a1da8a20977ac78e663988f9dba81902d4fb011a
Alejandro-Huaman <U201910144@upc.edu.pe>
2022-11-17 08:50:30 -0500

    add post fanatic details

--- src/app/pages/publication/post-list/post-list.component.ts
+++ src/app/pages/publication/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
+import { ActivatedRoute } from '@angular/router';
+import { FanaticService } from 'src/app/services/fanatic/fanatic.service';
 import { PublicationService } from 'src/app/services/publication/publication.service';
 @Component({
   selector: 'app-post-list',
@@ -11,14 +13,24 @@ export class PostListComponent implements OnInit {
   studentData: any;
   dataSource: MatTableDataSource<any>;
   haveInfo = false;
+  idurl!:number
+  isFanatic:boolean = false
 
-  constructor(private postService: PublicationService) {
+  constructor(private postService: PublicationService,private ActivatedRoute:ActivatedRoute, private fanaticService:FanaticService) {
     this.studentData = {}
     this.dataSource = new MatTableDataSource<any>();
     this.haveInfo = false;
   }
 
   ngOnInit(): void {
+    let id=parseInt(this.ActivatedRoute.snapshot.paramMap.get('id')!)    
+    this.idurl = id
+    console.log(this.idurl)
+
+    this.fanaticService.checkfanatic(this.idurl).subscribe((response:any) =>{
+      this.isFanatic = response
+      console.log(this.isFanatic)
+    });
   }
 
   getPosts(): void {
